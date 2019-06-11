288619cecef4cfe84a81e7807761a11835f72e9c
fabien <Fabien>
2019-06-11 22:12:24 +0200

    step 1.23.1

--- main.java
+++ main.java
@@ -1,6 +1,39 @@
+import java.util.*;
+
 class main {
 
 	public static void main(String args[]){
-		System.out.println("Salut");
+		if(args.length != 1){
+			System.out.println("Veuillez n'entrer qu'un argument");
+		}
+		else{
+			HashMap<String, Integer> map = new HashMap<String, Integer>();	
+			String text = args[0];
+			map = countWords(text);
+
+			for ( String key : map.keySet()) {
+				System.out.println( key + " = " + map.get(key) );
+			}
+		}
+	}
+
+	public static HashMap<String, Integer> countWords(String text){
+		HashMap<String, Integer> toReturn = new HashMap<String, Integer>();
+		Integer count = 0;
+		String[] textSplit = text.split(" ");
+		
+		for(String word : textSplit){
+			if(!toReturn.containsKey(word)){
+				toReturn.put(word,0);
+			}
+		}
+
+		for(String word : textSplit){
+			count = toReturn.get(word);
+			count++;
+			toReturn.replace(word,count);
+		}
+
+		return toReturn;
 	}
 }
